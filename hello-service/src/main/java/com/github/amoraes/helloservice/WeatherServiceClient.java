3479d2848fc31fc99c1fc83963255c6675d07dce
Alessandro Moraes <acampanhademoraes@infomedia.com.au>
2019-02-15 10:53:08 +1100

    fallback to cache

--- hello-service/src/main/java/com/github/amoraes/helloservice/WeatherServiceClient.java
+++ hello-service/src/main/java/com/github/amoraes/helloservice/WeatherServiceClient.java
@@ -1,5 +1,6 @@
 package com.github.amoraes.helloservice;
 
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface WeatherServiceClient {
 
     @GetMapping("/v1/weather")
+    @CachePut(cacheNames = "weather", key = "#p0", unless = "#result == null")
     public String getTemperature(@RequestParam("location") String location);
 
 }
--- hello-service/src/main/java/com/github/amoraes/helloservice/WeatherServiceClientFallback.java
+++ hello-service/src/main/java/com/github/amoraes/helloservice/WeatherServiceClientFallback.java
@@ -1,30 +1,15 @@
 package com.github.amoraes.helloservice;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
-import java.util.Random;
-
 @Component
-public class WeatherServiceClientFallback implements WeatherServiceClient {
+public class  WeatherServiceClientFallback implements WeatherServiceClient {
 
     @Override
+    @Cacheable(cacheNames = "weather", key = "#p0")
     public String getTemperature(String location) {
-        location = location.toLowerCase();
-        switch (location) {
-            case "sydney":
-                return randomTemperature(20,35);
-            case "brisbane":
-                return randomTemperature(25,40);
-            case "perth":
-                return randomTemperature(40,50);
-            default:
-                return randomTemperature(10,25);
-
-        }
+        return "not available";
     }
 
-    private String randomTemperature(int min, int max) {
-        int temperature = new Random().nextInt((max - min) + 1) + min;
-        return String.format("%d.0ºC", temperature);
-    }
 }
