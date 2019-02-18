8d6d530e1cea1de13fd2c36a11e91c8e20914771
Alessandro Moraes <acampanhademoraes@infomedia.com.au>
2019-02-18 11:51:12 +1100

    remove imports

--- gateway/src/main/java/com/github/amoraes/gateway/configuration/WebConfiguration.java
+++ gateway/src/main/java/com/github/amoraes/gateway/configuration/WebConfiguration.java
@@ -5,9 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration {
